36555e4244b5782d00070062bfc9e7661edafe80
jonguk-jeong <138849418+jonguk-jeong@users.noreply.github.com>
2026-05-26 15:38:48 +0900

    test 어노테이션 제거

--- src/test/kotlin/com/back/back2/S3ServiceImpl.kt
+++ src/test/kotlin/com/back/back2/S3ServiceImpl.kt
@@ -3,8 +3,6 @@ package com.back.back2
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 
-@Service
-@Profile("test")
 class S3ServiceImpl : S3Service {
     override fun getBucketNames(): List<String> {
         return listOf("")
