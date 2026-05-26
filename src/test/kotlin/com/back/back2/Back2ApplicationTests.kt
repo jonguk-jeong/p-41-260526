f502076afb698dd5b5d2950afaf5dbbebcea7413
jonguk-jeong <138849418+jonguk-jeong@users.noreply.github.com>
2026-05-26 14:26:51 +0900

    init

---
+++ src/test/kotlin/com/back/back2/Back2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.back.back2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Back2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
